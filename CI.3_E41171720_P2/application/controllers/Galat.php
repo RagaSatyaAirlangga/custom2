1622128e5434a5ee62413f9e5da67645d6de34d9
E41171720 <47999825+E41171720@users.noreply.github.com>
2019-03-19 11:13:56 +0700

    hgj
    hj

---
+++ CI.3_E41171720_P2/application/controllers/Galat.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+ 
+class Galat extends CI_Controller {
+    public function index()
+    {
+        echo "Halaman tidak ditemukan";
+    }
+ 
+}
---
+++ CI.3_E41171720_P2/application/controllers/Home.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * @package  : Ruby Pedia Offcial Site
+ * @author   : Fika Ridaul Maulayya <ridaulmaulayya@gmail.com>
+ * @since    : 2017
+ * @license  : https://www.rubypedia.com/
+ */
+class Home extends CI_Controller {
+
+    public function index()
+    {
+        echo '<h1>Ini adalah Controller Home</h1>';
+    }
+
+}
