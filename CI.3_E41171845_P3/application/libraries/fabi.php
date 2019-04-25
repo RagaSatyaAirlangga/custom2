fc1f7de53e8e3d1910ee2c3fdf1a620f41844d5c
yuniarfabiputra <47998680+yuniarfabiputra@users.noreply.github.com>
2019-04-25 14:57:46 +0700

    new

---
+++ CI.3_E41171845_P3/application/libraries/fabi.php
@@ -0,0 +1,11 @@
+<?php
+class fhrzl{
+    public function nama_saya(){
+        echo"Yuniar Fabi Putra";
+    }
+    public function nama_kamu($nama){
+        echo "Nama kamu adalah " .$nama." ";
+    }
+}
+
+?>
--- CI.3_E41171845_P3/application/views/v_tampil.php
+++ CI.3_E41171845_P3/application/views/v_tampil.php
@@ -1,10 +1,11 @@
+<!DOCTYPE html>
 <html>
 <head>
-	<title>Membuat CRUD dengan CodeIgniter</title>
+	<title>Membuat CRUD dengan CodeIgniter | MalasNgoding.com</title>
 </head>
 <body>
-	<center><h1>Membuat CRUD dengan CodeIgniter</h1></center>
-	<center><?php echo anchor('crud/tambah','Tambah Data'); ?></center>
+	<center><h1>Membuat CRUD dengan CodeIgniter | RajinNgoding.com</h1></center>
+	<center><?php echo anchor('crud/add','Tambah Data'); ?></center>
 	<table style="margin:20px auto;" border="1">
 		<tr>
 			<th>No</th>
@@ -12,6 +13,7 @@
 			<th>Alamat</th>
 			<th>Pekerjaan</th>
 			<th>Action</th>
+			
 		</tr>
 		<?php 
 		$no = 1;
@@ -24,7 +26,7 @@
 			<td><?php echo $u->pekerjaan ?></td>
 			<td>
 			      <?php echo anchor('crud/edit/'.$u->id,'Edit'); ?>
-                              <?php echo anchor('crud/hapus/'.$u->id,'Hapus'); ?>
+                 <?php echo anchor('crud/hapus/'.$u->id,'Hapus'); ?>
 			</td>
 		</tr>
 		<?php } ?>
