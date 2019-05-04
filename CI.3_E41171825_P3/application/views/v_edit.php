1c0d8efff4a81c6b583322e988ce8c9c97460658
Mithasq10 <47999705+Mithasq10@users.noreply.github.com>
2019-05-04 22:14:14 +0700

    hk
    gf

--- CI.3_E41171825_P3/application/views/v_edit.php
+++ CI.3_E41171825_P3/application/views/v_edit.php
@@ -9,7 +9,7 @@
 		<h3>Edit Data</h3>
 	</center>
 	<?php foreach($user as $u){ ?>
-	<form action="<?php echo base_url(). 'crud/update'; ?>" method="post">
+	<form action="<?php echo 'crud/update'; ?>" method="post">
 		<table style="margin:20px auto;">
 			<tr>
 				<td>Nama</td>
