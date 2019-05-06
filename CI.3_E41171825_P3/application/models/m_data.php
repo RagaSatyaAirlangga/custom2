8c15c85068d6f7567a62ce3172fb816027ccf75d
Mithasq10 <47999705+Mithasq10@users.noreply.github.com>
2019-05-06 13:07:21 +0700

    hj
    c

--- CI.3_E41171825_P3/application/models/m_data.php
+++ CI.3_E41171825_P3/application/models/m_data.php
@@ -7,7 +7,7 @@ class M_data extends CI_Model{
     function input_data($data,$table){
 		$this->db->insert($table,$data);
 	}
-	function hapus_data($where,$table){
+	function  hapus_data($where,$table){
 		$this->db->where($where);
 		$this->db->delete($table);
 	}
