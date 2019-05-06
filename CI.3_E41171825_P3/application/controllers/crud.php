b030bd78f9f0f8f887dccd04cf832b66533e178e
Mithasq10 <47999705+Mithasq10@users.noreply.github.com>
2019-05-06 15:07:34 +0700

    h
    re

--- CI.3_E41171825_P3/application/controllers/crud.php
+++ CI.3_E41171825_P3/application/controllers/crud.php
@@ -5,28 +5,28 @@ class Crud extends CI_Controller{
 	function __construct(){
 		parent::__construct();		
 		$this->load->model('m_data');
-                $this->load->helper('url');
+        $this->load->helper('url');
 	}
  
 	function index(){
-						$data['user'] = $this->m_data->tampil_data()->result();
-						$this->load->view('v_tampil',$data);
+		$data['user'] = $this->m_data->tampil_data()->result();
+		$this->load->view('v_tampil',$data);
     }
     function tambah(){
-						$this->load->view('v_input');
+		$this->load->view('v_input');
     }
     function tambah_aksi(){
-						$nama = $this->input->post('nama');
-						$alamat = $this->input->post('alamat');
-						$pekerjaan = $this->input->post('pekerjaan');
+		$nama = $this->input->post('nama');
+		$alamat = $this->input->post('alamat');
+		$pekerjaan = $this->input->post('pekerjaan');
  
-						$data = array(
-							'nama' => $nama,
-							'alamat' => $alamat,
-							'pekerjaan' => $pekerjaan
-							);
-						$this->m_data->input_data($data,'user');
-						redirect('crud/index');
+		$data = array(
+			'nama' => $nama,
+			'alamat' => $alamat,
+			'pekerjaan' => $pekerjaan
+		);
+		$this->m_data->input_data($data,'user');
+			redirect('crud/index');
 	}
 	function hapus($id){
 		$where = array('id' => $id);
@@ -58,3 +58,4 @@ class Crud extends CI_Controller{
 		redirect('crud/index');
 	}
 }
+?>
