f778e887c9013d15a4af5fe9bbfd7d744f2ede8b
E41171621 <47999730+E41171621@users.noreply.github.com>
2019-06-23 15:51:41 +0700

    ganti config
    customku

--- customku/application/config/config.php
+++ customku/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/CI.3_E41171621_P4/';
+$config['base_url'] = 'http://localhost/custom2/customku/';
 
 /*
 |--------------------------------------------------------------------------
